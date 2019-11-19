366c508ad12d0b344aef85f751118ef14ffe9795
Jon Richards <jrseti@gmail.com>
2019-11-19 08:26:07 -0800

    Working towards documentations, clean code. Work in progress

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jrtools
 Various Python tools and applications by Jon Richards
+
+Very much under development! Not actually useful yet!
---
+++ flux_densities/__init__.py
@@ -0,0 +1,4 @@
+from .flux_densities import get_sources
+from .flux_densities import get_source_coeffs
+from .flux_densities import get_jy
+from .flux_densities import create_html
