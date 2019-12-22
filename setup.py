fbb6fcec1ba0b1dfaac882b0629d25428ed90fc2
Jon Richards <jrseti@gmail.com>
2019-12-21 19:39:53 -0800

    Now uses requirements.txt

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ setup.py -- setup script for use of packages.
 """
 from setuptools import setup, find_namespace_packages
 
-install_requires = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='jrtools',
     packages=find_namespace_packages(include=['jrtools.*']),
     version='1.0',
-    install_requires=install_requires,
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'flux_densities=jrtools.flux_densities:main',
