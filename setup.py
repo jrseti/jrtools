192cd209f371f12a2ed20309152d199e2e086e2e
Jon Richards <jrseti@gmail.com>
2019-12-21 19:54:02 -0800

    setup packages correction.

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ from setuptools import setup, find_namespace_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+# Not using 'packages=find_namespace_packages(include=['jrtools.*'])',
+# but instead 'packages=['jrtools.flux_densities', 'jrtools.quick_chart']'.
+# The reason is that setuptools.find_namespace_packages is not available
+# on some pthon distributions. Listing them all manually is better anyway.
 setup(
     name='jrtools',
-    packages=find_namespace_packages(include=['jrtools.*']),
+    packages=['jrtools.flux_densities', 'jrtools.quick_chart'],
     version='1.0',
     install_requires=requirements,
     entry_points={
